990d6a5c728bb1742f4c1904875c4a62617570b9
Ваше Имя <martinushka536@yandex.ru>
2021-03-05 20:30:47 +0300

    little change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new, :create]
+
+  get 'contacts' => 'contacts#new'
+  resource :contacts, only: [:create]
   resources :articles
 end
