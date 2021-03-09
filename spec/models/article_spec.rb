2f2c6d23453e27a58e6eb7826ed2f8c00f4c3483
Ваше Имя <martinushka536@yandex.ru>
2021-03-09 10:11:44 +0300

    Update article spec to include validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+# проверяет валидации
 describe Article do
-  it { should have_many :comments }
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+# проверяет ассоциации
+  describe "assotiations" do
+    it { should have_many :comments }
+  end
 end
